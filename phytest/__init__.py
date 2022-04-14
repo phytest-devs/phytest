d5fb6becc0ab8d1775e2a2f769088dae64143a6c
Wytamma Wirth <wytamma.wirth@me.com>
2022-04-14 17:29:39 +1000

    :sparkles: add alignment and tree format options to cli

--- phytest/__init__.py
+++ phytest/__init__.py
@@ -10,8 +10,10 @@ from .helpers import trees as trees
 
 
 def pytest_addoption(parser):
-    parser.addoption("--alignment", "-A", action="store", default=None, help="alignment fasta file")
+    parser.addoption("--alignment", "-A", action="store", default=None, help="alignment file")
+    parser.addoption("--alignment-format", action="store", default='fasta', help="alignment file format")
     parser.addoption("--tree", "-T", action="store", default=None, help="tree file")
+    parser.addoption("--tree-format", action="store", default='newick', help="tree file format")
     parser.addoption(
         "--apply-fixes", action="store_true", default=None, help="automatically apply fixes where possible"
     )
@@ -38,21 +40,24 @@ def pytest_generate_tests(metafunc):
         fpth = Path(alignment_path)
         if not fpth.exists():
             raise FileNotFoundError(f"Unable to locate requested alignment file ({fpth})! 😱")
-        sequences = SeqIO.parse(alignment_path, 'fasta')
+        alignment_format = metafunc.config.getoption("--alignment-format")
+        sequences = SeqIO.parse(alignment_path, alignment_format)
         metafunc.parametrize("sequence", sequences, ids=lambda s: s.id)
 
 
 @pytest.fixture(scope="session", name="alignment")
 def _alignment_fixture(request):
     alignment_path = request.config.getoption("alignment")
-    alignment = AlignIO.read(alignment_path, 'fasta')
+    alignment_format = request.config.getoption("--alignment-format")
+    alignment = AlignIO.read(alignment_path, alignment_format)
     return alignment
 
 
 @pytest.fixture(scope="session", name="tree")
 def _tree_fixture(request):
     tree_path = request.config.getoption("tree")
-    tree = Phylo.read(tree_path, "newick")
+    tree_format = request.config.getoption("--tree-format")
+    tree = Phylo.read(tree_path, tree_format)
     return tree
 
 
--- phytest/cli.py
+++ phytest/cli.py
@@ -11,9 +11,15 @@ app = typer.Typer()
 def cli(
     testfile: Path = typer.Argument(..., help="Path to test file"),
     alignment: Optional[Path] = typer.Option(
-        None, "--alignment", "-a", dir_okay=False, exists=True, help="Path to sequence alignment in fasta format"
+        None, "--alignment", "-a", dir_okay=False, exists=True, help="Path to sequence alignment"
     ),
-    tree: Optional[Path] = typer.Option(None, "--tree", "-t", dir_okay=False, exists=True, help="Path to tree file"),
+    alignment_format: Optional[str] = typer.Option(
+        'fasta', "--alignment-format", dir_okay=False, exists=True, help="Sequence alignment format"
+    ),
+    tree: Optional[Path] = typer.Option(None, "--tree", "-t", dir_okay=False, exists=True, help="Tree file format"),
+    tree_format: Optional[str] = typer.Option(
+        'newick', "--tree-format", dir_okay=False, exists=True, help="Sequence alignment format"
+    ),
     report: Optional[bool] = typer.Option(False, "--report", "-r", help="Generate an html report"),
     verbose: Optional[bool] = typer.Option(False, "--verbose", "-v", help="Verbose output"),
 ):
@@ -22,8 +28,10 @@ def cli(
         args.extend(["-ra", "--tb=no", "--no-header"])
     if alignment is not None:
         args.extend(["--alignment", alignment])
+        args.extend(["--alignment-format", alignment_format])
     if tree is not None:
         args.extend(["--tree", tree])
+        args.extend(["--tree-format", tree_format])
     if report:
         args.extend(["--html=report.html", "--self-contained-html"])
 
