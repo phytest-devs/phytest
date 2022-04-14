fbf1753a433575084e11611fba3b0d8f7dd6e96d
Simon Mutch <smutch@unimelb.edu.au>
2022-04-14 16:22:19 +1000

    Add more cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import pytest
 from typer.testing import CliRunner
 
 from phytest.cli import app
@@ -9,3 +10,10 @@ def test_cli_help():
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
     assert "TESTFILE  Path to test file  [required]" in result.stdout
+
+
+def test_cli_no_input_file(request: pytest.FixtureRequest):
+    result = runner.invoke(app, [str(request.path.parent / "input/testfile1.py")])
+    assert result.exit_code == 0
+    assert "testfile1.py" in result.stdout
+    assert "1 passed" in result.stdout
