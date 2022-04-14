dddc7e2086fb981063c479d38a62e2118c7d6ea2
Wytamma Wirth <wytamma.wirth@me.com>
2022-04-14 10:31:52 +1000

    :white_check_mark: Improve test cov

--- tests/test_alignments.py
+++ tests/test_alignments.py
@@ -1,9 +1,10 @@
 import pytest
+from Bio import AlignIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-from Bio import AlignIO
 
 from phytest import alignments
+
 
 def test_assert_valid_alphabet():
     dna = SeqRecord(
@@ -20,16 +21,16 @@ def test_assert_valid_alphabet():
     )
     alignments.assert_valid_alphabet(dna)
     with pytest.raises(AssertionError):
-        alignments.assert_valid_alphabet(dna, alphabet="ABCDE")    
+        alignments.assert_valid_alphabet(dna, alphabet="ABCDE")
 
     alignments.assert_valid_alphabet(protein, alphabet="ACDEFGHIKLMNPQRSTVWYXBZJ")
     with pytest.raises(AssertionError):
-        alignments.assert_valid_alphabet(protein)    
+        alignments.assert_valid_alphabet(protein)
 
 
 def test_assert_length():
     dna = SeqRecord(
-        Seq("A"*100),
+        Seq("A" * 100),
         id="DNAID",
         name="TEST",
         description="Test dna sequence",
@@ -42,14 +43,14 @@ def test_assert_length():
         alignments.assert_length(dna, min=101)
         alignments.assert_length(dna, max=99)
 
+
 def test_assert_count():
     dna = SeqRecord(
-        Seq("ATG"*100),
+        Seq("ATG" * 100),
         id="DNAID",
         name="TEST",
         description="Test dna sequence",
     )
-    alignments.assert_count(dna, base='A', count=100)
     alignments.assert_count(dna, base='A', count=100)
     alignments.assert_count(dna, base='A', min=99)
     alignments.assert_count(dna, base='A', max=101)
@@ -58,14 +59,14 @@ def test_assert_count():
         alignments.assert_count(dna, base='A', min=101)
         alignments.assert_count(dna, base='A', max=99)
 
+
 def test_assert_count_Ns():
     dna = SeqRecord(
-        Seq("ATGN"*100),
+        Seq("ATGN" * 100),
         id="DNAID",
         name="TEST",
         description="Test dna sequence",
     )
-    alignments.assert_count_Ns(dna, count=100)
     alignments.assert_count_Ns(dna, count=100)
     alignments.assert_count_Ns(dna, min=99)
     alignments.assert_count_Ns(dna, max=101)
@@ -77,12 +78,11 @@ def test_assert_count_Ns():
 
 def test_assert_count_gaps():
     dna = SeqRecord(
-        Seq("ATG-"*100),
+        Seq("ATG-" * 100),
         id="DNAID",
         name="TEST",
         description="Test dna sequence",
     )
-    alignments.assert_count_gaps(dna, count=100)
     alignments.assert_count_gaps(dna, count=100)
     alignments.assert_count_gaps(dna, min=99)
     alignments.assert_count_gaps(dna, max=101)
@@ -91,19 +91,26 @@ def test_assert_count_gaps():
         alignments.assert_count_gaps(dna, min=101)
         alignments.assert_count_gaps(dna, max=99)
 
+
 def test_assert_alignment_width():
     alignment_path = 'examples/data/invalid.fasta'
     alignment = AlignIO.read(alignment_path, 'fasta')
     alignments.assert_alignment_width(alignment, width=100)
+    alignments.assert_alignment_width(alignment, min=99)
+    alignments.assert_alignment_width(alignment, max=101)
     with pytest.raises(AssertionError):
         alignments.assert_alignment_width(alignment, width=99)
+        alignments.assert_alignment_width(alignment, min=101)
+        alignments.assert_alignment_width(alignment, max=99)
 
 
 def test_assert_alignment_length():
     alignment_path = 'examples/data/invalid.fasta'
     alignment = AlignIO.read(alignment_path, 'fasta')
     alignments.assert_alignment_length(alignment, length=3)
+    alignments.assert_alignment_length(alignment, min=2)
+    alignments.assert_alignment_length(alignment, max=4)
     with pytest.raises(AssertionError):
         alignments.assert_alignment_length(alignment, length=1)
-
-   
+        alignments.assert_alignment_length(alignment, min=4)
+        alignments.assert_alignment_length(alignment, max=2)
