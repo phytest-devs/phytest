10a72ddc6ba839bce3361c02a4432a506add402e
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-05-14 21:54:37 +1000

    :cop: using shortcut to read newick strings in tests

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -8,9 +8,9 @@ from phytest.utils import default_date_patterns, PhytestWarning, PhytestAssertio
 
 
 def test_assert_tree_number_of_tips():
-    treedata = "(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);"
-    handle = StringIO(treedata)
-    tree = Tree.read(handle, "newick")
+    tree = Tree.read_str(
+        "(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);"
+    )
     tree.assert_number_of_tips(tips=7, min=6, max=8)
     with pytest.raises(AssertionError):
         tree.assert_number_of_tips(tips=1)
@@ -21,16 +21,16 @@ def test_assert_tree_number_of_tips():
 
 
 def test_assert_tree_is_bifurcating():
-    treedata = "(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);"
-    handle = StringIO(treedata)
-    tree = Tree.read(handle, "newick")
+    tree = Tree.read_str(
+        "(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);"
+    )
     tree.assert_is_bifurcating()
 
 
 def test_assert_tree_is_monophyletic():
-    treedata = "(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);"
-    handle = StringIO(treedata)
-    tree = Tree.read(handle, "newick")
+    tree = Tree.read_str(
+        "(Bovine:0.69395,(Hylobates:0.36079,(Pongo:0.33636,(G._Gorilla:0.17147, (P._paniscus:0.19268,H._sapiens:0.11927):0.08386):0.06124):0.15057):0.54939, Rodent:1.21460);"
+    )
     print(tree.root)
     tips = [tip for tip in tree.get_terminals() if tip.name in ("P._paniscus", "H._sapiens")]
     tree.assert_is_monophyletic(tips)
@@ -40,9 +40,9 @@ def test_assert_tree_is_monophyletic():
 
 
 def test_assert_tree_total_branch_length():
-    treedata = "(Bovine:1,(Hylobates:1,(Pongo:1,(G._Gorilla:1, (P._paniscus:1,H._sapiens:1):1):1):1):1, Rodent:1);"
-    handle = StringIO(treedata)
-    tree = Tree.read(handle, "newick")
+    tree = Tree.read_str(
+        "(Bovine:1,(Hylobates:1,(Pongo:1,(G._Gorilla:1, (P._paniscus:1,H._sapiens:1):1):1):1):1, Rodent:1);"
+    )
     tree.assert_total_branch_length(length=11, min=10, max=12)
     with pytest.raises(AssertionError):
         tree.assert_total_branch_length(length=1)
