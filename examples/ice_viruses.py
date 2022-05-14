2eb12e6f3b80cfbf75a8900741af7b120edb6279
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-05-14 21:38:14 +1000

    :zap: adding assert for root date range in ice viruses example

--- examples/ice_viruses.py
+++ examples/ice_viruses.py
@@ -11,4 +11,5 @@ def test_alignment(alignment: Alignment):
 
 def test_root_to_tip(tree: Tree):
     # Evolutionary rate of influenza is on the order of 10^-3
-    tree.assert_root_to_tip(min_r_squared=0.5, min_rate=0.001, max_rate=0.009)
+    tree.assert_root_to_tip(min_r_squared=0.5, min_rate=0.001, max_rate=0.009, min_root_date=1852, max_root_date=1853)
+
